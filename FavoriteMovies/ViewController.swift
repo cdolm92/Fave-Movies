27b80ee6478c21f7defea0d324ac5c4d23a74c6e
cdolm92 <cdolm92@gmail.com>
2016-03-09 23:53:04 -0500

    first init

---
+++ FavoriteMovies/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  FavoriteMovies
+//
+//  Created by Christella on 3/5/16.
+//  Copyright © 2016 Christella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return MovieCell()
+    }
+
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+
+
+}
+
